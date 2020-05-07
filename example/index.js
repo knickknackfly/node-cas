e369b1811f73116f2cdadf25433b0d92e1c43415
Julianzeng <zenghengjun@xiaomi.com>
2020-05-07 20:54:26 +0800

    clone from https://www.npmjs.com/package/connect-cas3

---
+++ example/index.js
@@ -0,0 +1,156 @@
+var path = require('path');
+var http = require('http');
+var process = require('process');
+var Express = require('express');
+var session = require('express-session');
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var CasClient = require('../index');
+var ejs = require('ejs');
+var morgan = require('morgan');
+
+var app = new Express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser('here is some secret'));
+
+var MemoryStore = require('session-memory-store')(session);
+
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'I am a secret',
+  name: 'jssessionid',
+  store: new MemoryStore()
+}));
+
+app.engine('.html', ejs.renderFile);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, './views'));
+
+var demoParams = {
+  appId: '900007430',
+  pid: '1',
+  type: 8,
+  appKey: 'BXEKfudgcgVDBb8k'
+};
+
+// CAS config
+// =============================================================================
+var casClient = new CasClient({
+  debug: true,
+  ignore: [
+    /\/ignore/
+  ],
+  match: [],
+  servicePrefix: 'http://10.17.86.87:8080',
+  serverPath: 'http://rdmdev.oa.com',
+  paths: {
+    validate: '/cas/validate',
+    serviceValidate: '/buglycas/serviceValidate',
+    proxy: '/buglycas/proxy',
+    login: '/buglycas/login',
+    logout: '/buglycas/logout',
+    proxyCallback: 'http://10.17.86.87:8080/buglycas/proxyCallback',
+    restletIntegration: '/buglycas/v1/tickets'
+  },
+  redirect: false,
+  gateway: false,
+  renew: false,
+  ssoff: true,
+  cache: {
+    enable: true,
+    ttl: 5 * 60 * 1000,
+    filter: [
+      // /betaserverpre\.et\.wsd\.com/
+    ]
+  },
+  fromAjax: {
+    header: 'x-client-ajax',
+    status: 418
+  },
+  restletIntegration: {
+    demo1: {
+      trigger: function(req) {
+        console.log('Checking restletIntegration rules');
+        return true;
+      },
+      params: {
+        username: demoParams.appId + '_' + demoParams.pid,
+        from: 'http://10.17.86.87:8080/cas/validate',
+        type: demoParams.type,
+        password: JSON.stringify({ appId: demoParams.appId + '_' + demoParams.pid, appKey: demoParams.appKey })
+      }
+    }
+  }
+});
+
+app.use(casClient.core());
+app.get('/logout', casClient.logout());
+
+app.get('/', function(req, res) {
+  res.render('index.ejs');
+});
+
+
+app.get('/api', function(req, res) {
+  var service = 'http://betaserverpre.et.wsd.com/betaserverv2';
+  req.getProxyTicket(service + '/shiro-cas', function(err, pt) {
+    if (err) {
+      console.log('getProxyTicket error');
+      console.log(err);
+      return res.status(500).send(JSON.stringify(err));
+    }
+
+    console.log('sending request to ', service + '/file/4d6960ff-c11f-4ba8-baed-af1a2f95bffe/info?' + require('query-string').stringify({
+        ticket: pt
+      }));
+
+    require('../lib/utils').getRequest(service + '/file/4d6960ff-c11f-4ba8-baed-af1a2f95bffe/info?' + require('query-string').stringify({
+        ticket: pt
+      }), function(err, response) {
+      res.send(response);
+    });
+  })
+});
+
+app.get('/clear', function(req, res) {
+  req.clearRestlet();
+  console.log('this means it is sync!');
+  res.send('ok');
+});
+
+var server = http.createServer(app);
+
+
+server.listen(8080, function(err) {
+  if (err) throw err;
+  console.log('App is now listening to port 8080.');
+});
+
+process.stdin.resume();//so the program will not close instantly
+
+function exitHandler(options, err) {
+  if (options.cleanup) console.log('clean');
+  if (err) console.log(err.stack);
+  if (options.exit) process.exit();
+}
+
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
+
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
+
+
+var obj = {
+  "http://ciserver.et.wsd.com/ciserver3.0": true,
+  "http://processservice.et.wsd.com/processservice": false,
+  "http://svnmanage.sdet.wsd.com/svnmange": false,
+  "http://productserver.et.wsd.com/productserver": false,
+  "http://cimonitor.et.wsd.com/monitorserver": false
+};
---
+++ index.js
@@ -0,0 +1,175 @@
+var _ = require('lodash');
+
+var validate = require('./lib/validate');
+var proxyCallback = require('./lib/proxyCallback');
+var authenticate = require('./lib/authenticate');
+var slo = require('./lib/slo');
+var getProxyTicket = require('./lib/getProxyTicket');
+var getProxyTicketThroughRestletReq = require('./lib/getProxyTicketThroughRestletReq');
+var PTStroe = require('./lib/ptStroe');
+var utils = require('./lib/utils');
+var clearRestletTGTs = require('./lib/clearRestletTGTs');
+var url = require('url');
+var globalStoreCache = require('./lib/globalStoreCache');
+
+var DEFAULT_OPTIONS = {
+  debug: false,
+  ignore: [],
+  match: [],
+  servicePrefix: '',
+  serverPath: '',
+  paths: {
+    validate: '/cas/validate',
+    serviceValidate: '/cas/serviceValidate',
+    proxy: '/cas/proxy',
+    login: '/cas/login',
+    logout: '/cas/logout',
+    proxyCallback: '/cas/proxyCallback'
+  },
+  redirect: false,
+  gateway: false,
+  renew: false,
+  slo: true,
+  // Is proxy-ticket cacheable
+  cache: {
+    enable: false,
+    ttl: 5 * 60 * 1000, // In millisecond
+    filter: []
+  },
+  fromAjax: {
+    header: 'x-client-ajax',
+    status: 418
+  },
+  // demo:
+  // [{
+  //   trigger: function(req) {
+  //     return false;
+  //   },
+  //   params: {
+  //     username: '',
+  //     password: ''
+  //   }
+  // }]
+  restletIntegration: {}
+};
+
+function ConnectCas(options) {
+  if (!(this instanceof ConnectCas)) return new ConnectCas(options);
+
+  this.options = _.merge({}, DEFAULT_OPTIONS, options);
+
+  if (!this.options.servicePrefix || !this.options.serverPath) throw new Error('Unexpected options.service or options.serverPath!');
+
+  this.ptStore = new PTStroe(_.merge({}, this.options.cache, { debug: this.options.debug }));
+
+  this.logger = this.options.logger || function(req, type) {
+      return console[type].bind(console[type]);
+    };
+
+  var pgtURI = url.parse(this.options.paths.proxyCallback);
+
+  this.proxyCallbackPathName = (pgtURI.protocol && pgtURI.host) ? pgtURI.pathname : this.options.paths.proxyCallback;
+}
+
+ConnectCas.prototype.LoggerFactory = function() {
+};
+
+ConnectCas.prototype.core = function() {
+  var options = this.options;
+  var that = this;
+
+  return function(req, res, next) {
+    if (!req.sessionStore) throw new Error('You must setup a session store before you can use CAS client!');
+    if (!req.session) throw new Error('Unexpected req.session ' + req.session);
+
+    var pathname = req.path;
+    var method = req.method;
+
+    var logger = {
+      info: that.logger(req, 'log'),
+      error: that.logger(req, 'error'),
+      warn: that.logger(req, 'warn'),
+      log: that.logger(req, 'log')
+    };
+
+    var matchedRestletIntegrateRule;
+
+    if (options.restletIntegration) {
+      if (options.paths.restletIntegration) {
+        for (var i in options.restletIntegration) {
+          if (options.restletIntegration[i] && typeof options.restletIntegration[i].trigger === 'function' && options.restletIntegration[i].trigger(req)) {
+            matchedRestletIntegrateRule = i;
+            req.clearRestlet = clearRestletTGTs.bind(null, options, logger);
+            break;
+          }
+        }
+      } else {
+        logger.warn("options.restletIntegration is set, but options.paths.restletIntegration is undefined! Maybe you forget to set all your paths.")
+      }
+    }
+
+    if (options.paths.proxyCallback) {
+      req.getProxyTicket = function(targetService, disableCache, callback) {
+        return matchedRestletIntegrateRule ? getProxyTicketThroughRestletReq.call(that, req, targetService, matchedRestletIntegrateRule, options.restletIntegration[matchedRestletIntegrateRule], logger, typeof disableCache === 'function' ? disableCache : callback) :
+          getProxyTicket.call(that, req, targetService, disableCache, logger, callback);
+      };
+    }
+
+    if (matchedRestletIntegrateRule) return next();
+
+    if (utils.shouldIgnore(req, options)) return next();
+
+    if (method === 'GET') {
+      switch (pathname) {
+        case options.paths.validate:
+          return validate(req, res, next, options, logger);
+        case that.proxyCallbackPathName:
+          return proxyCallback(req, res, next, options, logger);
+        default:
+          return authenticate(req, res, next, options, logger);
+      }
+    } else if (method === 'POST' && pathname === options.paths.validate && options.slo) {
+      return slo(req, res, next, options, logger);
+    }
+
+    next();
+  };
+};
+
+ConnectCas.prototype.logout = function() {
+  var options = this.options;
+
+  return function(req, res, next) {
+    if (!req.session) {
+      return res.redirect('/');
+    }
+    // Forget our own login session
+
+    if (req.session.destroy) {
+      req.session.destroy();
+    } else {
+      // Cookie-based sessions have no destroy()
+      req.session = null;
+    }
+
+    // Send the user to the official campus-wide logout URL
+    return res.redirect(utils.getPath('logout', options));
+  };
+};
+
+ConnectCas.prototype.getPath = function(name, req) {
+  return utils.getPath(name, this.options, req);
+};
+
+ConnectCas.prototype.destroyRestletIntegrations = function() {
+  var stores = globalStoreCache.getAll();
+
+  for (var i in stores) {
+    utils.deleteRequest(utils.getPath('restletIntegration') + '/' + stores[i], function(err, response) {
+      if (err || !response || response.statusCode != 200 || response.status != 200) return console.error('Error when delete restletIntegartion-created TGT!');
+      console.log('Delete restletIntegartion-created TGT succeed!', stores[i]);
+    });
+  }
+};
+
+module.exports = ConnectCas;
